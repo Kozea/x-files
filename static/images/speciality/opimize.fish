024a4ef844aa49d72dc0189b14ba0632e236a44d
Clément Plasse <clement.plasse@kozea.fr>
2018-01-09 10:55:41 +0100

    It's not an image

--- static/images/speciality/opimize.fish
+++
@@ -1,4 +0,0 @@
-#!/bin/env fish
-
-# https://stackoverflow.com/questions/7261855/recommendation-for-compressing-jpg-files-with-imagemagick#7262050
-for i in *.jpg;convert -strip -interlace Plane -quality 85% $i $i;end
